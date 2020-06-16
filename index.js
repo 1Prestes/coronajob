ddc7ad4de47c5682087f4a862af1d526d8dc3a85
1Prestes <dijango_ilha@hotmail.com>
2020-06-16 18:44:14 -0300

    ok

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 const sqlite = require('sqlite');
-const dbConnection = sqlite.open('banco.sqlite', { Promise });
+const dbConnection = sqlite.open(path.resolve(__dirname, 'banco.sqlite'), { Promise });
 
 const port = process.env.PORT || 3000
+
+app.set('views'.path.join(__dirname, 'views'));
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
